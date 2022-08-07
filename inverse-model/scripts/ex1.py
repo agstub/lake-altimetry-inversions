91d3816bbd11b6b70e8b1ce2ee9662b4fa2f310c
Aaron Stubblefield <ags2189@columbia.edu>
2022-08-07 14:17:10 -0400

    added comments, modified plotting in example script

--- inverse-model/scripts/ex1.py
+++ inverse-model/scripts/ex1.py
@@ -1,14 +1,13 @@
+# example script running the inversion on some synthetic data
+# and plotting the results
+
 import sys
 sys.path.insert(0, '../source')
 
 from inversion import invert
-from operators import forward_w
-from params import x,y,t,Nx,Ny,Nt,data_dir,H,x0,y0,t0
+from params import x,y,t,Nt,data_dir,H,x0,y0,t0
 import numpy as np
-from kernel_fcns import ifftd,fftd
-from conj_grad import norm
 from post_process import calc_dV_w,calc_dV_h
-from localization import localize
 import os
 if os.path.isdir('../pngs')==False:
     os.mkdir('../pngs')    # make a directory for the results.
@@ -16,53 +15,59 @@ if os.path.isdir('../pngs')==False:
 
 import matplotlib.pyplot as plt
 
+# load synthetic elevation data (h_obs) and "true" basal vertical velocity (w_true)
 h_obs = np.load(data_dir+'/h.npy')
 w_true = np.load(data_dir+'/w_true.npy')
-
-
-# generate timeseries over the lake to estimate
-h_mean = np.copy(h_obs)
-h_mean[np.sqrt(x**2+y**2)>5] = 0
-h_mean = np.sum(h_mean,axis=(-1,-2))/np.sum(h_mean!=0,axis=(-1,-2))
-i_est = np.argmax(np.abs(h_mean))
-
-h_temp = 1+0*h_obs[i_est,:,:]
-h_temp[np.abs(h_obs[i_est,:,:])<0.1] = 0
-h_bdry = np.multiply.outer(np.ones(Nt),h_temp)
-
-w_bdry = 1+0*x
-w_bdry[np.sqrt(x**2+y**2)>1e4/H] = 0
 
 w_inv,h_fwd,mis = invert(h_obs,1e-3)
 
 print('misfit norm = '+str(mis))
 
-dV_true = calc_dV_w(w_true,w_bdry)
-dV_alt = calc_dV_h(h_obs,h_bdry)
-dV_inv = calc_dV_w(w_inv,w_bdry)
+# lake boundary estimate (h_bdry) = 1 inside lake, 0 outside lake
+h_bdry = 1+0*x
+h_bdry[np.abs(h_obs)<0.1] = 0
+h_bdry = np.mean(h_bdry,axis=0)
 
+# boundary of true basal vertical velocity ("true" lake boundary)
+w_bdry = 1+0*x
+w_bdry[np.sqrt(x**2+y**2)>1e4/H] = 0
+
+# boundary of basal vertical velocity inversion (estimated lake boundary)
+inv_bdry = 1+0*x
+inv_bdry[np.abs(w_inv)<0.01*np.max(np.abs(w_inv))] = 0
+inv_bdry = np.mean(inv_bdry,axis=0)
+
+# calculate volume change time series:
+dV_true = calc_dV_w(w_true,w_bdry)      # true volume change (from w_true)
+dV_alt = calc_dV_h(h_obs,h_bdry)        # volume change estimate from h_obs alone
+dV_inv = calc_dV_w(w_inv,inv_bdry)        # volume change from inversion
+
+# calculate the estimated vs true highstand time for illustration
 hs_true = np.argmax((dV_true[0:int(Nt/2.)+10]))
+hs_inv = np.argmax((dV_inv[0:int(Nt/2.)+10]))
 hs_alt = np.argmax((dV_alt[0:int(Nt/2.)+10]))
 
-
+# plot everything
 xy_str = H/1e3
-
 for i in range(Nt):
     print('image '+str(i+1)+' out of '+str(Nt))
 
     plt.figure(figsize=(12,7))
 
     plt.subplot(121)
-    plt.plot(t0[0:i],dV_true[0:i]*(H**2)/1e9,color='royalblue',linewidth=3,label=r'true sol.')
-    plt.plot(t0[0:i],dV_alt[0:i]*(H**2)/1e9,color='crimson',linestyle='-.',linewidth=3,label=r'altimetry')
+    plt.plot(t0[0:i],dV_true[0:i]*(H**2)/1e9,color='forestgreen',linewidth=3,label=r'true sol.')
     plt.plot(t0[0:i],dV_inv[0:i]*(H**2)/1e9,color='k',linestyle='--',linewidth=3,label=r'inversion')
+    plt.plot(t0[0:i],dV_alt[0:i]*(H**2)/1e9,color='crimson',linestyle='-.',linewidth=3,label=r'altimetry')
+
 
     if i >= hs_true:
-        plt.plot([t0[hs_true]],[dV_true[hs_true]*(H**2)/1e9],color='royalblue',marker='o',markersize=10)
+        plt.plot([t0[hs_true]],[dV_true[hs_true]*(H**2)/1e9],color='forestgreen',marker='o',markersize=10)
+    if i >= hs_inv:
+        plt.plot([t0[hs_inv]],[dV_inv[hs_inv]*(H**2)/1e9],color='k',marker='o',markersize=10)
     if i >= hs_alt:
         plt.plot([t0[hs_alt]],[dV_alt[hs_alt]*(H**2)/1e9],color='crimson',marker='o',markersize=10)
 
-    plt.ylabel(r'$\Delta V$ (km$^2$)',fontsize=20)
+    plt.ylabel(r'$\Delta V$ (km$^3$)',fontsize=20)
     plt.xlim(0,t0[-1])
     plt.ylim(-0.2,0.4)
     plt.xlabel(r'$t$ (yr)',fontsize=20)
@@ -74,7 +79,7 @@ for i in range(Nt):
     plt.subplot(222)
     plt.annotate(r'$h_\mathrm{obs}$',fontsize=20,xy=(30,-37))
     p=plt.contourf(xy_str*x0,xy_str*y0,h_obs[i,:,:].T,cmap='coolwarm',levels=np.arange(-1,1,0.2),extend='both')
-    plt.contour(xy_str*x0,xy_str*y0,h_bdry[0,:,:].T,colors='k',linewidths=2,levels=[1e-10])
+    plt.contour(xy_str*x0,xy_str*y0,h_bdry[:,:].T,colors='crimson',linestyles='-.',linewidths=3,levels=[1e-10])
     plt.gca().xaxis.set_ticklabels([])
     plt.gca().yaxis.tick_right()
     plt.ylabel(r'$y$ (km)',fontsize=20)
@@ -86,7 +91,9 @@ for i in range(Nt):
 
     plt.subplot(224)
     plt.annotate(r'$w_\mathrm{inv}$',fontsize=20,xy=(30,-37))
-    plt.contourf(xy_str*x0,xy_str*y0,w_inv[i,:,:].T,cmap='coolwarm',extend='both',levels=np.arange(-5,5,1))
+    p=plt.contourf(xy_str*x0,xy_str*y0,w_inv[i,:,:].T,cmap='coolwarm',extend='both',levels=np.arange(-5,5,1))
+    plt.contour(xy_str*x0,xy_str*y0,inv_bdry[:,:].T,colors='k',linestyles='--',linewidths=3,levels=[1e-1])
+    plt.contour(xy_str*x0,xy_str*y0,w_bdry[0,:,:].T,colors='forestgreen',linewidths=3,levels=[1e-10])
     plt.ylabel(r'$y$ (km)',fontsize=20)
     plt.xlabel(r'$x$ (km)',fontsize=20)
     plt.gca().yaxis.tick_right()
--- inverse-model/scripts/make_synth_data.py
+++ inverse-model/scripts/make_synth_data.py
@@ -1,23 +1,33 @@
+# this script makes synthetic elevation data from a prescribed basal vertical
+# velocity anomaly. the forward operator is applied to the velocity anomaly,
+# producing the elevation anomaly, then the elevation anomaly is localized
+# to remove off-lake deformation (in practice these are interpreted as regional trends
+# but a small component may be due to the lake activity), and some noise is added
+# to the synthetic elevation field
+
 import sys
 sys.path.insert(0, '../source')
 
 import numpy as np
 import os
 
+# make a directory for the data
 if os.path.isdir('../data_synth_lin')==False:
-    os.mkdir('../data_synth_lin')    # make a directory for the results.
+    os.mkdir('../data_synth_lin')
 
-
+# define the scalar parameters in the problem
+# in practice we might use means of some of these fields around the lake
 H = np.array([1000])          # ice thickness over the lake
 beta = np.array([1e8])        # basal drag coeff. near the lake
-eta = np.array([1e13])        # viscosity
-u = np.array([0])             # background flow speed
+eta = np.array([1e13])        # viscosity near the lake
+u = np.array([0])             # background flow speed near the lake
 
-
-t0 = np.linspace(0,6,100)              # time
+# define coordinate arrays
+t0 = np.linspace(0,6,100)                     # time
 x0 = np.linspace(-40,40,101)*1000/H.mean()    # x coordinate
 y0 = np.linspace(-40,40,101)*1000/H.mean()    # y coordinate
 
+# save everything so far in the data directory
 np.save('../data_synth_lin/t.npy',t0)
 np.save('../data_synth_lin/x.npy',x0)
 np.save('../data_synth_lin/y.npy',y0)
@@ -26,19 +36,23 @@ np.save('../data_synth_lin/beta.npy',beta)
 np.save('../data_synth_lin/H.npy',H)
 np.save('../data_synth_lin/u.npy',u)
 
+# import some functions to produce the synthetic elevation anomaly
 from params import t,x,y,Nt,Nx,Ny
 from operators import forward_w
 from localization import localize
 from kernel_fcns import ifftd
 from conj_grad import norm
 
-L = 10
-sigma = (1000/H)*L/3
-
+# set basal vertical velocity anomaly to an oscillating gaussian
+sigma = (10000.0/H)/3.0
 w_true = 5*np.exp(-0.5*(sigma**(-2))*(x**2+y**2))*np.sin(4*np.pi*t/np.max(t))
 
+# produce synthetic elevation anomaly by applying the forward operator
+# (returns fft of elevation), inverse fourier-transforming the results,
+# and removing the off-lake component ("localize" function)
 h = localize(ifftd(forward_w(w_true)).real)
 
+# add some noise
 noise_h = np.random.normal(size=(Nt,Nx,Ny))
 noise_level = 0.25
 h_obs = h + noise_level*norm(h)*noise_h/norm(noise_h)
