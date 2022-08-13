1dc3a0b8d6370e85f4b51e69fa8934598e840a68
Aaron Stubblefield <ags2189@columbia.edu>
2022-08-13 16:23:00 -0400

    changed defualt params

--- nonlinear-model/source/params.py
+++ nonlinear-model/source/params.py
@@ -31,7 +31,7 @@ B = (2**((n-1.0)/(2*n)))*B0        # coefficient in weak form (Pa s^{1/n})
 rho_i = 917.0                      # density of ice (kg/m^3)
 rho_w = 1000.0                     # density of water (kg/m^3)
 g = 9.81                           # gravitational acceleration (m/s^2)
-C = 1e6                            # sliding law friction coefficient (Pa s/m)^1/n
+C = 1e4                            # sliding law friction coefficient (Pa s/m)^1/n
 
 # numerical parameters
 eps_p = 1.0e-14                    # penalty method parameter for unilateral condition
@@ -60,7 +60,7 @@ dy = Hght/Ny
 # time-stepping parameters
 t_period = 4*3.154e7               # oscillation period (secs; yr*sec_per_year)
 t_final = 1.5*t_period             # final time
-nt_per_cycle = 2000                # number of timesteps per oscillation
+nt_per_cycle = 1000                # number of timesteps per oscillation
 nt = int(t_final/t_period*nt_per_cycle) # number of time steps
 dt = t_final/nt                    # timestep size
 
